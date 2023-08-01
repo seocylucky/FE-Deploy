89afa2dfbdd7d5a2fcca5c565cac9fb35236d0fd
seocylucky <chaeyeon2285@naver.com>
2023-08-01 19:50:15 +0900

    환경변수 설정

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
                 </div>
                 <div className="card back">
                     <div>
-                        <span className="name">서채연</span>
-                        <span className="position">FE</span>
+                        <span className="name">{process.env.REACT_APP_NAME}</span>
+                        <span className="position">{process.env.REACT_APP_PART}</span>
                     </div>
-                    <a className="github" href="https://github.com/seocylucky"></a>
+                    <a className="github" href={process.env.REACT_APP_GITHUB}></a>
                 </div>
             </div>
         </>
